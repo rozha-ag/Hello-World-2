6935d8a4faac2cfa6ddd662788e6124971d8afc2
u1974644 <57265928+u1974644@users.noreply.github.com>
2020-11-16 23:38:16 +0000

    Demo

---
+++ src/HelloWorld.java
@@ -0,0 +1,18 @@
+/**
+ * A demo class.
+ *
+ * @author Rozha Aghaei
+ * @version September 2020
+ */
+
+public class HelloWorld {
+    /**
+     * A demo method.
+     * @param  args command line arguments (not used)
+     */
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
---
+++ src/HelloWorldTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloWorldTest {
+    @Test
+    public void passTest() {}
+
+    @Test
+    public void failTest() {
+        fail();
+    }
+
+}
